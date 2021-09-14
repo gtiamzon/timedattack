0be6302f23ebc984f9df657f154b9d7f2ba54233
gtiamzon <gerald.tiamzon@yahoo.com>
2021-09-13 23:35:01 -0700

    stuck on laptime form

--- controllers/laptime_controller.js
+++ controllers/laptime_controller.js
@@ -4,11 +4,30 @@ const router = express.Router();
 const { Track, User, Car } = require("../models");
 
 // laptime create show page
-router.get('/new', (req, res) => {
-  const context= {};
-  return res.render('new_lap', context);
+router.get('/new', async (req, res, next) => {
+  try {
+    const foundCars = await Car.find({username: req.session.currentUser.id});
+    const allTracks = await Track.find({});
+    const context = {
+      cars: foundCars,
+      tracks: allTracks
+    };
+    return res.render('new_lap', context);
+  } catch(console) {
+    console.log(error);
+    req.error = error;
+    return next();
+  }
 });
 
+//LAPTIME FUNCTIONAL create
+// router.post('/new', (req, res) => {
+//   try {
 
+//   } catch (error) {
+//     const context = {error},
+//     return res.render('/laptime/new', context)
+//   }
+// });
 
 module.exports = router;
